4025ba47d05a27bbe1fce9c513fef2980bff661b
kittysans <cannotclarify@gmail.com>
2024-03-28 16:04:14 +0700

    update

--- main.js
+++ main.js
@@ -1,66 +1,131 @@
 const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
-const quoteDisplayElement = document.getElementById('quoteDisplay')
+const quoteDisplayElement = document.getElementById('quote-display')
+const resetButton = document.getElementById('reset')
 
+const timerBar = document.getElementById('timer-bar')
+const timerLeft = document.getElementById('timeLeft')
+const mistakeElement = document.getElementById('mistake')
+const WpmSpan = document.getElementById('wpm')
 
-let inputValue = []
+let quoteValue = []
+let mistake = 0
+
+let countTimer = false
+let timer = 30
+let currTime = 0
+let ratio
+
+mistakeElement.innerText = mistake
+timerLeft.innerText = timer
+
+let timeRunning
+
+function processWPM() {
+    let typingCorr = 0
+
+    quoteDisplayElement.querySelectorAll('span').forEach((span, i) => {
+        if (quoteValue[i] && span.innerText == quoteValue[i] && span.innerText != ' ') typingCorr += 1
+    });
+    WpmSpan.innerText = Math.round((typingCorr / 5) / timer * 60)
+}
+
+function startTimer() {
+    timerBar.style.width = `0px`
+
+    timeRunning = setInterval(function() {
+        // timer bar
+        if (currTime < timer) {
+            currTime += 1
+            ratio = (currTime / timer) * 100
+           
+            timerBar.style.width = `${(ratio / 100) * document.querySelector('.display').clientWidth}px`
+        }
+        else {
+            processWPM()
+            clearInterval(timeRunning)
+            window.removeEventListener('keydown', updateInputValue)
+        }
+
+        // time left
+        timerLeft.innerText = timer - currTime
+    }, 1000);
+}
 
 function updateInputValue(e) {
-    const arrayQuote = quoteDisplayElement.querySelectorAll('span')
-
-    // delete the last of input value
-    if (e.key === 'Backspace') {
-        inputValue.pop()
+    if (!countTimer) {
+        startTimer()
+        countTimer = true
     }
 
-    // put spacebar
-    if (e.key === ' ') {
-        inputValue.push(' ')
+    let inputValue = e.key
+    if (inputValue == 'Shift') return
+    if (inputValue == 'Backspace' && quoteValue) quoteValue.pop()
+    else {
+        if (quoteDisplayElement.querySelectorAll('span')[quoteValue.length].innerText != inputValue && quoteDisplayElement.querySelectorAll('span')[quoteValue.length].innerText != ' ') mistake += 1
+        quoteValue.push(inputValue)
+
+        let soundClickKeyboard = new Audio('./sounds/mixkit-slide-click-1130.wav')
+        soundClickKeyboard.play()
     }
 
-    // check if this value is a-z
-    if (/^[a-z,.\"'-;?]$/i.test(e.key)) {
-        inputValue.push(e.key)
-    }
 
-    arrayQuote.forEach((characterSpan, index) => {
-        characterSpan.classList.remove('curr')
+    quoteDisplayElement.querySelectorAll('span').forEach((chr, i) => {
+        chr.classList.remove('curr')
 
-        if (inputValue[index] == null) {
-            characterSpan.classList.remove('correct')
-            characterSpan.classList.remove('incorrect')
-        } else if (inputValue[index] === characterSpan.innerText) {
-            characterSpan.classList.add('correct')
-            characterSpan.classList.remove('incorrect')
-        } else {
-            characterSpan.classList.remove('correct')
-            characterSpan.classList.add('incorrect')
+        if (quoteValue[i] == null) {
+            chr.classList.remove('corr')
+            chr.classList.remove('incorr')
+        }
+        else if (chr.innerText == quoteValue[i]) {
+            chr.classList.add('corr')
+            chr.classList.remove('incorr')
+        }
+        else {
+            chr.classList.remove('corr')
+            chr.classList.add('incorr')
         }
     });
 
-    if (arrayQuote[inputValue.length]) arrayQuote[inputValue.length].classList.add('curr')
-
-    if (arrayQuote.length == inputValue.length) renderNewQuote()
+    mistakeElement.innerText = mistake
+    if (quoteDisplayElement.querySelectorAll('span')[quoteValue.length]) quoteDisplayElement.querySelectorAll('span')[quoteValue.length].classList.add('curr')
+    if (quoteDisplayElement.querySelectorAll('span').length == quoteValue.length) renderNewQuote()
 }
 
 function getRandomQuote() {
-  return fetch(RANDOM_QUOTE_API_URL)
-    .then(response => response.json())
-    .then(data => data.content)
+    return fetch(RANDOM_QUOTE_API_URL)
+        .then(response => response.json())
+        .then(data => data.content)
 }
 
 async function renderNewQuote() {
-  const quote = await getRandomQuote()
+    const quote = await getRandomQuote()
 
-  quote.split('').forEach((character, index) => {
-    const characterSpan = document.createElement('span')
-    characterSpan.innerText = character
-    if (index == 0) {
-        characterSpan.classList.add('curr')
-    }
-    quoteDisplayElement.appendChild(characterSpan)
-  })
-
-  window.addEventListener('keydown', updateInputValue)
+    let notAddCurrEffect = false
+    quote.split('').forEach(character => {
+        const characterSpan = document.createElement('span')
+        characterSpan.innerText = character
+        if (!notAddCurrEffect) characterSpan.classList.add('curr'), notAddCurrEffect = true
+        quoteDisplayElement.appendChild(characterSpan)
+    })
+  
+    window.addEventListener('keydown', updateInputValue)
 }
-
+  
 renderNewQuote()
+
+resetButton.addEventListener('click', () => {
+    // reset value
+    quoteValue = []
+    mistake = 0
+
+    countTimer = false
+    currTime = 0
+    timerBar.style.width = `0`
+
+    mistakeElement.innerText = mistake
+    timerLeft.innerText = timer
+
+    quoteDisplayElement.innerText = null
+
+    renderNewQuote()
+})
